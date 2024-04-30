ed12670d33f474c97f3317652fe5c781246e5f48
André <alcbcoelho@gmail.com>
2024-04-30 02:42:37 -0300

    Implemented insert method in SellerDaoJDBC class

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,18 +5,42 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        SellerDao sellerDao = DaoFactory.createSellerDao();
+        try {
+            SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== TEST 1:  Seller findById ===");
-        Seller seller = sellerDao.findById(3);
-        System.out.println(seller);
+            System.out.println("=== TEST 1:  Seller findById ===");
+            Seller seller = sellerDao.findById(3);
+            System.out.println(seller);
 
-        System.out.println("\n=== TEST 2:  Seller findByDepartment ===");
-        List<Seller> sellerList = sellerDao.findByDepartment(new Department(1, "Computers"));
-        sellerList.forEach(System.out::println);
+            System.out.println("\n=== TEST 2:  Seller findByDepartment ===");
+            Department dep = new Department(1, "Computers");
+            List<Seller> sellerList = sellerDao.findByDepartment(dep);
+            sellerList.forEach(System.out::println);
+
+            System.out.println("\n=== TEST 3:  Seller findAll ===");
+            sellerList = sellerDao.findAll();
+            sellerList.forEach(System.out::println);
+
+            System.out.println("\n=== TEST 4:  Seller insert ===");
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            seller = new Seller(
+                    null,
+                    "Donald Brown",
+                    "donald@gmail.com",
+                    sdf.parse("31/12/1994"),
+                    1000.0,
+                    dep
+            );
+            sellerDao.insert(seller);
+            System.out.println("Inserted! New id = " + seller.getId());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -6,12 +6,11 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -23,7 +22,41 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
 
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                    + "VALUES "
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS     // retorna no formato ResultSet coluna com ID do elemento inserido
+            );
+
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setDouble(5, obj.getDepartment().getId());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                rs = st.getGeneratedKeys();
+                if (rs.next()) {
+                    obj.setId(rs.getInt(1));    // populando a instância de Seller que serve de argumento p/
+                                                            // o método com o ID gerado pela operação de inserção ao BD
+                }
+                DB.close(rs);   // fechando o ResultSet por aqui porque ele foi criado dentro de um bloco 'if', só existindo
+                                // portanto aqui dentro
+            } else
+                throw new DbException("Unexpected error! No rows affected!");
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.close(st);
+        }
     }
 
     @Override
@@ -85,7 +118,39 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(
+                    "SELECT *, department.Name as DepName FROM seller "
+                    + "INNER JOIN department "
+                    + "ON DepartmentId = department.id "
+                    + "ORDER BY seller.Id"
+            );
+
+            List<Seller> sellers = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>(); // vamos usar este map para fazer um algoritmo (ln 105-109)
+            // que possa nos auxiliar a não criar instâncias repetidas de um mesmo Department para cada Seller
+            // que é adicionado na lista.
+
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+                sellers.add(instantiateSeller(rs, dep));
+            }
+            return (sellers.size() > 0) ? sellers : null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.close(rs);
+            DB.close(st);
+        }
     }
 
     @Override
